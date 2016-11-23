49d1c00282b79d991f18974f9678cb4cc16ccd46
AaronMorelli <aaronmorelli@zoho.com>
2016-11-23 14:43:52 -0800

    Create FileUsageMaps.sql

---
+++ TSQL/FileMgmt/FileUsageMaps.sql
@@ -0,0 +1,3 @@
+http://slavasql.blogspot.com/2016/08/srinking-database-file-with-help-of.html
+  uses spatial functions combined with dm_db_page_allocations to map out the used space w/in a file, and even see which objects
+  have allocations near the end.
